515929d3a0c6a973fd855785a8e20eea7e1e1b63
Mario <airbomberofaqua@gmail.com>
2024-10-09 11:02:50 -0400

    almost done

--- index.js
+++ index.js
@@ -1,1 +1,74 @@
+//
 
+const multiplesOfSixAndNine = () => {
+  let i = 0;
+  for (let i = 0; i <= 100; i++) {
+    if (i % 6 === 0 && i % 9 === 0) {
+      console.log(i);
+    }
+  }
+};
+
+//multiplesOfSixAndNine();
+
+const greaterNum = (num1, num2) => {
+  if (num1.isInteger === num1 && num2.isInteger === num2) {
+    return "null";
+  } else if (num1 === num2) {
+    return "both  intigers  are equal";
+  } else if (num1 > num2) {
+    return num1;
+  } else if (num2 > num1) {
+    return num2;
+  } else {
+    return "null";
+  }
+};
+
+// console.log(greaterNum(10, 7)); //returns 10
+// console.log(greaterNum(1.14, 1.14)); //returns "both integers are equal"
+// console.log(greaterNum("21", 21)); //returns null
+// console.log(greaterNum("21", "21")); //returns null
+
+const sumOfFourAndSix = () => {
+  let sum = 0;
+  let i = 0;
+  while (i != 999) {
+    i++;
+    if (i % 4 === 0 && i % 6 === 0) {
+      sum += i;
+    }
+  }
+  return sum;
+};
+
+//console.log(sumOfFourAndSix());
+
+const oddAndEvenToN = (num) => {
+  let i = 0;
+  while (i <= num) {
+    if (i % 2 === 0) {
+      console.log(`${i} is even`);
+    } else {
+      console.log(`${i} is odd`);
+    }
+    i++;
+  }
+};
+
+//oddAndEvenToN(5);
+
+const sumOfNotDivisbleByTen = () => {
+  let sum = 0;
+  let i = 0;
+  while (i != 1000) {
+    i++;
+    if (i % 10 !== 0) {
+      sum += i;
+    }
+  }
+
+  return sum;
+};
+
+console.log(sumOfNotDivisbleByTen());
